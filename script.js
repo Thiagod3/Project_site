e77ea0ef6992516e3dc654cf58531620d4e42458
Thiagod3 <thiago.vilela3@outlook.com>
2023-04-07 15:39:36 -0300

    agrfoi_commit

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
-const CLIENT_ID = '349911634958-o9uai4bcnrr7b8s0bb31hhs0ki7ca2ee.apps.googleusercontent.com';
-const API_KEY = 'AIzaSyC2igx3OKvYRl2855iat7eMlyr9VfZDwCo';
-const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
-const SCOPES = 'https://www.googleapis.com/auth/drive.readonly';
+ // Lista de URLs das imagens
+ var imagens = [
+  "fotos/4lan.jpg",
+  "fotos/Adolfz.jpg",
+  "fotos/Axtlol.jpg",
+  "fotos/Ayel.jpg",
+  "fotos/Bianquinha.jpg",
+  "fotos/BlackOutz.jpg",
+  "fotos/Boo.jpg",
+  "fotos/BRTT.jpg",
+  "fotos/Bueirinho.jpg",
+  "fotos/Cellbit.jpg",
+  "fotos/Cerol.jpg",
+  "fotos/Chris Paiva.jpg",
+  "fotos/CSR.jpg",
+  "fotos/Daniels.jpg",
+  "fotos/Defante.jpg",
+  "fotos/DMenor11.jpg",
+  "fotos/Fir3zera.jpg",
+  "fotos/Fnx.jpg",
+  "fotos/gragolandia2.jpg",
+  "fotos/Gratis150ml.jpg",
+  "fotos/Hastaad.jpg",
+  "fotos/Hayshii.jpg",
+  "fotos/hudisonamorim.jpg",
+  "fotos/IShowSpeed.jpg",
+  "fotos/JeanMago.jpg",
+  ];
 
-let imageIndex = 0;
-let images = [];
+  let intervalId = setInterval(function trocarImagem() {
 
-function atualizarImagem() {
-  // Autenticação e obtenção do token de acesso
-  gapi.load('client:auth2', function () {
-    gapi.client.init({
-      apiKey: API_KEY,
-      clientId: CLIENT_ID,
-      discoveryDocs: DISCOVERY_DOCS,
-      scope: SCOPES,
-    }).then(function () {
-      return gapi.auth2.getAuthInstance().signIn();
-    }).then(function () {
-      const token = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
-      
-      // Obtenção da lista de arquivos de imagem do Google Drive
-      fetch(`https://www.googleapis.com/drive/v3/files?q=mimeType%3D%27image%2Fjpeg%27%20or%20mimeType%3D%27image%2Fpng%27&fields=files(id%2Cname%2CwebContentLink)&orderBy=createdTime%20desc&key=${API_KEY}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then(response => response.json())
-      .then(data => {
-        images = data.files;
-        imageIndex = Math.floor(Math.random() * images.length);
-        const imageUrl = images[imageIndex].webContentLink;
-        const imageElement = document.getElementById('image');
-        imageElement.src = imageUrl;
-        imageElement.alt = images[imageIndex].name;
-      });
-    });
-  });
-}
+    var indice = Math.floor(Math.random() * imagens.length);
+    var img = document.getElementById("random-img");
+    img.src = imagens[indice];
+  }, 2000);
+
+
